baa2058b8b303544e0cce8ab692c469f4354aec5
Aamer-Rabih <Aamer.rabih@yahoo.com>
2019-04-28 23:53:16 +0300

    add template

---
+++ resources/views/dashboard/layout/master.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>تركي هوم</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Baloo+Bhaijaan&amp;subset=arabic" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  </head>
+
+  <body>
+
+    @yield('header')
+
+    @yield('sidbar')
+
+
+
+
+
+
+
+    @yield('footer')
+   
+
+    <!-- jQuery (Bootstrap JS plugins depend on it) -->
+    <script src="js/jquery-2.1.4.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
---
+++ resources/views/dashboard/layout/particles/header.blade.php
@@ -0,0 +1,17 @@
+@extends('master')
+
+@section('header')
+
+	<header>
+      <nav id="header-nav" class="navbar navbar-default">
+          <div class="navbar-header">
+            <a href="index.html" class="pull-right">
+              <div id="admin-logo">
+                تركي هوم
+              </div>
+            </a>
+          </div>
+      </nav>
+    </header>
+
+@stop
