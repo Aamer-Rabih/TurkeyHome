2b3b2c29134fc1094b28672e831c6997b73330ee
Aamer-Rabih <Aamer.rabih@yahoo.com>
2019-06-01 02:49:05 +0300

    fixing

--- resources/views/admin/layouts/partials/sidebar.blade.php
+++ resources/views/admin/layouts/partials/sidebar.blade.php
@@ -8,7 +8,7 @@
         <li><a href="{{ route('course.index') }}">الدورات<i class="fa fa-angle-double-left pull-left"></i></a></li>
         <li><a href="{{route('subject.index')}}">المواد<i class="fa fa-angle-double-left pull-left"></i></a></li>
         <li><a href="{{route('unit.index')}}">الوحدات الدرسية<i class="fa fa-angle-double-left pull-left"></i></a></li>
-        <li><a href="#">الدروس<i class="fa fa-angle-double-left pull-left"></i></a></li>
+        <li><a href="{{route('lesson.index')}}">الدروس<i class="fa fa-angle-double-left pull-left"></i></a></li>
         <li><a href="{{route('carousel.index')}}">إدارة القلاب<i class="fa fa-angle-double-left pull-left"></i></a></li>
         <li><a href="{{route('showlesson.index')}}">الدروس الأستعراضية<i class="fa fa-angle-double-left pull-left"></i></a></li>
         <li><a href="{{route('studentthank.index')}}">تشكرات الطلاب<i class="fa fa-angle-double-left pull-left"></i></a></li>
